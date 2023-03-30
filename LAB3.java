b4f79ae576a1fae15a5c7ce6b26b77fdd33e057b
methus <methus.dumr@kmutt.ac.th>
2023-03-30 22:44:09 +0700

    first commit

---
+++ LAB3.java
@@ -0,0 +1,16 @@
+package myexception2;
+
+public class LAB3 {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Statement 1");
+            System.out.println(1 / 0);
+            System.out.println("Statement 3");
+        } catch (IndexOutOfBoundsException e) {
+        } finally {
+            System.out.println("Statement 4");
+        }
+        System.out.println("Statement 5");
+    }
+}
